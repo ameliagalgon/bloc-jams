1189b630d7854b90d1b4418176804d0821dde393
ameliagalgon <amg617@lehigh.edu>
2017-07-27 13:37:24 -0400

    Assignment 20 jQuery Buzz Library: complete

--- scripts/album.js
+++ scripts/album.js
@@ -167,6 +167,28 @@ var previousSong = function(){
   currentlyPlayingCell.html(pauseButtonTemplate);
 }
 
+var togglePlayFromPlayerBar = function(){
+  if(currentSoundFile.isPaused()){
+    /*
+    Change the song number cell from a play button to a pause button
+    Change the HTML of the player bar's play button to a pause button
+    Play the song
+    */
+    getSongNumberCell(currentlyPlayingSongNumber).html(pauseButtonTemplate);
+    $playPauseButton.html(playerBarPauseButton);
+    currentSoundFile.play();
+  } else if(currentSoundFile.isPaused() === false){
+    /*
+    Change the song number cell from a pause button to a play button
+    Change the HTML of the player bar's pause button to a play button
+    Pause the song
+    */
+    getSongNumberCell(currentlyPlayingSongNumber).html(playButtonTemplate);
+    $playPauseButton.html(playerBarPlayButton);
+    currentSoundFile.pause();
+  }
+};
+
 // Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -182,9 +204,11 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
    setCurrentAlbum(albumPicasso);
    $previousButton.click(previousSong);
    $nextButton.click(nextSong);
+   $playPauseButton.click(togglePlayFromPlayerBar);
 });
